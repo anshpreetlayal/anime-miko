9a8cc0d27b43055f986a67831a120ed840f9c4c4
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-11 13:20:07 -0500

    editing

--- components/AnimeDetails.js
+++ components/AnimeDetails.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
+import { fetchAnimeList } from '../components/api';
 
 const AnimeDetails = ({ anime }) => {
   return (
     <View>
-      <Text>{anime.title}</Text>
-      {/* Display other anime details here */}
+      <Text>Title: {anime.title}</Text>
+      <Text>Genres: {anime.genres.join(', ')}</Text>
+      <Text>Episodes: {anime.episodes}</Text>
+      <Text>Rating: {anime.rating}</Text>
+      {/* Add more details as needed */}
     </View>
   );
 };
 
-export default AnimeDetails;
+const AnimeDetailsScreen = ({ route }) => {
+  const [animeDetails, setAnimeDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const animeId = route.params.animeId;
+        const animeData = await fetchAnimeList(animeId); 
+        setAnimeDetails(animeData); 
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDetails();
+  }, [route.params.animeId]);
+
+  return (
+    <View>
+      {animeDetails ? (
+        <AnimeDetails anime={animeDetails} />
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </View>
+  );
+};
+
+export default AnimeDetailsScreen;
