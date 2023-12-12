b2655254179fb8c437e3913590a72be9a0009772
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-11 19:28:28 -0500

    editing

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../components/HomeScreen';
-import DetailsScreen from '../components/AnimeDetails';
-import SettingsScreen from '../components//SettingsScreen';
-import AnimeDetailsScreen from './components/AnimeDetails';
+import IntroScreen from './components/IntroScreen';
+import HomeScreen from './components/HomeScreen';
+import SettingsScreen from './components/SettingsScreen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={AnimeDetails} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Navigator
+        initialRouteName="anime-miko"
+        screenOptions={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: '#8B4513',
+          },
+          headerTintColor: '#FFFFFF', 
+          headerTitleStyle: {
+            fontWeight: 'bold', 
+            fontFamily: 'Georgia', 
+          },
+        }}
+      >
+        <Stack.Screen
+          name="anime-miko"
+          component={IntroScreen}
+          options={{
+            headerTitle: 'anime-miko',
+            headerTitleAlign: 'center',
+          }}
+        />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
-
