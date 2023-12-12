faa971b7113b9e149dea72dea55d6e3739b45730
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-11 19:27:30 -0500

    editing

--- components/AnimeList.js
+++ components/AnimeList.js
@@ -1,50 +1,81 @@
-import React, { useRef } from 'react';
-import { View, Text, FlatList, TouchableOpacity, PanResponder, Animated } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, ActivityIndicator, Image, StyleSheet } from 'react-native';
 
-const AnimeList = ({ animeData, onPressItem }) => {
-  const pan = useRef(new Animated.ValueXY()).current;
+const API_URL = 'https://api.jikan.moe/v4/anime?q=&sfw';
 
-  const panResponder = PanResponder.create({
-    onStartShouldSetPanResponder: () => true,
-    onPanResponderMove: Animated.event(
-      [
-        null,
-        {
-          dx: pan.x,
-          dy: pan.y,
-        },
-      ],
-      { useNativeDriver: false }
-    ),
-    onPanResponderRelease: () => {
-    },
-  });
+const AnimeList = ({ onPressItem }) => {
+  const [loading, setLoading] = useState(true);
+  const [animeData, setAnimeData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        setAnimeData(data.data); 
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => onPressItem(item.id)}>
-      <Text>{item.title}</Text>
+    <TouchableOpacity onPress={() => onPressItem(item.mal_id)}>
+      <View style={styles.itemContainer}>
+        <Image source={{ uri: item.images.jpg.image_url }} style={styles.image} />
+        <Text style={styles.title}>{item.title}</Text>
+      </View>
     </TouchableOpacity>
   );
 
   return (
-    <View>
-      <Animated.View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: 'skyblue',
-          transform: [{ translateX: pan.x }, { translateY: pan.y }],
-        }}
-        {...panResponder.panHandlers}
-      />
-
-      <FlatList
-        data={animeData}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
+    <View style={styles.container}>
+      {loading ? (
+        <ActivityIndicator size="large" style={styles.loader} />
+      ) : (
+        <FlatList
+          data={animeData}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.mal_id.toString()}
+        />
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    marginBottom: 10,
+    padding: 10,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  loader: {
+    marginTop: 20,
+  },
+});
 
 export default AnimeList;
