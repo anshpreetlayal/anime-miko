b47f3786e6db334a9aea29073e4791cd729ceeb2
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-03-13 18:30:03 -0400

    updated animedetails

--- components/AnimeDetails.js
+++ components/AnimeDetails.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ActivityIndicator, StyleSheet, Image, ScrollView } from 'react-native';
+import { View, Text, ActivityIndicator, StyleSheet, Image, ScrollView, TouchableOpacity } from 'react-native';
 
 const API_URL = 'https://api.jikan.moe/v4/anime?q=&sfw';
 
 const AnimeDetailsScreen = ({ route }) => {
   const [animeDetails, setAnimeDetails] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const animeId = route.params.animeId;
@@ -14,12 +15,16 @@ const AnimeDetailsScreen = ({ route }) => {
     async function fetchAnimeDetails(animeId) {
       try {
         const response = await fetch(`${API_URL}/${animeId}`); 
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
         const data = await response.json();
         setAnimeDetails(data);
         setLoading(false);
       } catch (error) {
         console.error(error);
         setLoading(false);
+        setError(true);
       }
     }
   }, [route.params.animeId]);
@@ -28,8 +33,10 @@ const AnimeDetailsScreen = ({ route }) => {
     <View style={styles.container}>
       {loading ? (
         <ActivityIndicator size="large" />
+      ) : error ? (
+        <Text style={styles.errorText}>Failed to fetch data. Please try again later.</Text>
       ) : animeDetails ? (
-        <ScrollView>
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
           <Image source={{ uri: animeDetails.images.jpg.image_url }} style={styles.image} />
           <Text style={styles.title}>{animeDetails.title}</Text>
           <Text style={styles.info}>Episodes: {animeDetails.episodes}</Text>
@@ -37,7 +44,7 @@ const AnimeDetailsScreen = ({ route }) => {
           <Text style={styles.info}>Status: {animeDetails.status}</Text>
           <Text style={styles.info}>Rating: {animeDetails.rating}</Text>
           <Text style={styles.info}>Synopsis: {animeDetails.synopsis}</Text>
-=        </ScrollView>
+        </ScrollView>
       ) : (
         <Text>No data available</Text>
       )}
@@ -50,6 +57,10 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
   },
+  scrollViewContent: {
+    flexGrow: 1,
+    alignItems: 'center',
+  },
   image: {
     width: '100%',
     height: 300,
@@ -60,10 +71,16 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10,
+    textAlign: 'center',
   },
   info: {
     fontSize: 16,
     marginBottom: 5,
+    textAlign: 'center',
+  },
+  errorText: {
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
 
